28f64c0acfa60cefd968643fc8811aa1a79f5e7f
yunsungjoong <yun_sj94@naver.com>
2024-07-17 09:31:02 +0900

    캐패시터 스타트

---
+++ ionic_capacitor/android/app/src/main/java/com/sk/pe/toktok/todo/MainActivity.java
@@ -0,0 +1,5 @@
+package com.sk.pe.toktok.todo;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ ionic_capacitor/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.sk.pe.toktok.todo',
+  appName: '과제완수',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
