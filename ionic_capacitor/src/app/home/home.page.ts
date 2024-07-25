107b553790eb3a6194c3615506b48215fc5df74f
yunsungjoong <yun_sj94@naver.com>
2024-07-25 17:08:30 +0900

    수정

--- ionic_capacitor/src/app/home/home.page.ts
+++ ionic_capacitor/src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Platform } from '@ionic/angular';
+import { Platform, ToastController } from '@ionic/angular';
 import { Capacitor } from '@capacitor/core';
 @Component({
   selector: 'app-home',
@@ -9,7 +9,7 @@ import { Capacitor } from '@capacitor/core';
 export class HomePage {
   phoneNumber: string = '';
   
-  constructor(private platform:Platform) {}
+  constructor(private platform:Platform, private toastController: ToastController) {}
 
   makeCall() {
     if(this.phoneNumber) {
@@ -17,6 +17,8 @@ export class HomePage {
       if(this.platform.is('hybrid')) {
         window.location.href = `tel:${this.phoneNumber}`;
       }
+    } else {
+      this.showToast('전화번호를 입력해주세요');
     }
   }
 
@@ -25,6 +27,17 @@ export class HomePage {
       if (this.platform.is('hybrid'))
       // 문자 메시지 앱을 열고 입력된 전화번호로 문자 보내기
         window.location.href = `sms:${this.phoneNumber}`;
+    }  else {
+      this.showToast('전화번호를 입력해주세요');
     }
+  }
+
+  async showToast(message: string) {
+    const toast = await this.toastController.create({
+      message : message,
+      duration: 2000,
+      position: 'bottom'
+    });
+    toast.present();
   }
 }
